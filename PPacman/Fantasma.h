dfac7fcd7f6a985ff5f6f709728a736b940c29a5
Raisa Isabel Saravia Peralta <raisa_99_11@hotmail.com>
2021-04-06 22:01:53 -0400

    cambios sdl

--- PPacman/Fantasma.h
+++ PPacman/Fantasma.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <SDL.h>
+#include "Movimientos.h"
 class Fantasma
 {
+private:
+	DireccionMovimiento direccionActual;
+	DireccionMovimiento direccionSiguiente;
+
+	SDL_Point posicionActual;
+public:
+	DireccionMovimiento getDireccionActual() { return direccionActual; }
+	DireccionMovimiento setDireccionActual(DireccionMovimiento _direccionActual) { direccionActual = _direccionActual; }
+
+	DireccionMovimiento getDireccionSiguiente() { return direccionSiguiente; }
+	DireccionMovimiento setDireccionSiguiente(DireccionMovimiento _direccionSiguiente) { direccionSiguiente = _direccionSiguiente; }
+	
 };
 
---
+++ PPacman/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Pacman.h"
+using namespace std;
+
+int main() {
+	Pacman pacmansito;
+
+
+	cout << "prueba" << endl;
+	return 0;
+}
