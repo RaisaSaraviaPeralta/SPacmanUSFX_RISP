6e2a639ee244fc3dfab83b4b854ea6e77c174d05
Raisa Isabel Saravia Peralta <raisa_99_11@hotmail.com>
2021-04-07 09:42:41 -0400

    sdl point

--- PPacman/Fruta.h
+++ PPacman/Fruta.h
@@ -1,19 +1,16 @@
 #pragma once
+#include <SDL.h>
 class Fruta
 {
 private:
 	int color;
-	int posicionX;
-	int posicionY;
+	SDL_Point posicion;
 public:
 	int getColor() { return color; }
 	void setColor(int _color) { color = _color; }
 
-	int getPosicionX() { return posicionX; }
-	void setPosicionX(int _posicionX) { posicionX = _posicionX; }
-
-	int getPosicionY() { return posicionY; }
-	void setPosicionY(int _posicionY) { posicionY = _posicionY; }
+	SDL_Point getPosicion() { return posicion; }
+	void setPosicion(SDL_Point _posicion) {  posicion = _posicion; }
 
 	//metodo accesor de propiedades de tipo
 
